f430a7ebd1b7b6dc78f9020d0514e1d426eba40d
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-01-28 23:27:34 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -32,27 +32,6 @@ def welcome_assignment_answers(question):
 # Complete all the questions.
 
 
-if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decode a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What is the SHA256 hashing value to the following message: 'NYU Computer Networking'- Use SHA256 hash generator and use the answer in your code"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-    print(welcome_assignment_answers(debug_question))
-
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
 #"Are encoding and encryption the same? - Yes/No":
