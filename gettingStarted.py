e459d76fcf5d934a1a5e694094704dd5c5395d9d
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-01-28 21:07:02 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,14 +18,12 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
-
     else:
         # you should understand why this else case should be included
         # what happens if there is a typo in one of the questions?
         # maybe put something here to flag an issue
         answer = " "
     return (answer)
-
 
 if __name__ == "__main__":
     # use this space to debug and verify that the program works
