4d4813d0865a8abe6a684a2b57958ddbb62b8e7e
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-01-28 21:48:22 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -45,3 +45,13 @@ if __name__ == "__main__":
     debug_question = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
     print(welcome_assignment_answers(debug_question))
 
+#Questions:
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+#"Are encoding and encryption the same? - Yes/No":
+#"Is it possible to decrypt a message without a key? - Yes/No":
+#"Is it possible to decode a message without a key? - Yes/No":
+#"Is a hashed message supposed to be un-hashed? - Yes/No":
+#"What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
+#"Is MD5 a secured hashing algorithm? - Yes/No":
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
