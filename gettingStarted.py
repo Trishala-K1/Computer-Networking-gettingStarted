41800fd994c63e36517a283a7f9fa90545e0613f
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-01-28 21:15:19 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -21,7 +21,7 @@ def welcome_assignment_answers(question):
         # you should understand why this else case should be included
         # what happens if there is a typo in one of the questions?
         # maybe put something here to flag an issue
-        answer = " "
+        answer = "Invalid Question."
     return (answer)
 
 if __name__ == "__main__":
